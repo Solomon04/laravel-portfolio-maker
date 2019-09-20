48c4e28e6ceec1ac46b87c0f1f5641ed6ffc9e6a
Solomon04 <Antoinesolomon5@gmail.com>
2019-09-20 18:58:06 -0500

    footer update

--- resources/views/grayscale/inc/footer.blade.php
+++ resources/views/grayscale/inc/footer.blade.php
@@ -3,6 +3,6 @@
     <div class="container">
         <p>Copyright &copy; {{$user->name}} {{\Carbon\Carbon::today()->year}}</p>
         <br>
-        <small>Made with <a href="#">Laravel Portfolio Maker</a> </small>
+        <small>Made with <a href="https://github.com/Solomon04/laravel-portfolio-maker">Laravel Portfolio Maker</a> </small>
     </div>
 </footer>
